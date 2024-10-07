89b9b83e1e53175eaec65afa8053e848159f5462
Connor Rhodes <connor@rhodes.contact>
2024-10-07 17:11:53 -0500

    ternary operator test

---
+++ ternary.rb
@@ -0,0 +1,12 @@
+# if true
+#   p 'TRUE'
+# else
+#   p 'FALSE'
+# end
+
+age = 19
+
+# Using the ternary operator to assign a value based on a condition
+status = age >= 18 ? "adult" : "minor"
+
+puts "The person is an #{status}."
