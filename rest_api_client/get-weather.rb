edf82b1b96a7449d733d06f456268a8dd07557c3
Connor Rhodes <connor@rhodes.contact>
2024-12-19 21:05:35 -0600

    parse only response body

--- rest_api_client/get-weather.rb
+++ rest_api_client/get-weather.rb
@@ -8,7 +8,8 @@ api_key = '**********'
 
 geo = hp.get("http://api.openweathermap.org/geo/1.0/zip?zip=78758,US&appid=#{api_key}")
 
-weather = hp.get("https://api.openweathermap.org/data/2.5/weather?lat=#{geo["lat"]}&lon=#{geo["lon"]}&units=imperial&appid=#{api_key}", format: :plain)
-weather = JSON.parse(weather, symbolize_names: true)
+weather = hp.get("https://api.openweathermap.org/data/2.5/weather?lat=#{geo["lat"]}&lon=#{geo["lon"]}&units=imperial&appid=#{api_key}")
+
+weather = JSON.parse(weather.body, symbolize_names: true)
 
 binding.pry
