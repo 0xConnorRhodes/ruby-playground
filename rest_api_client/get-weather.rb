a087cbacbd4d3682fcddc7d241fec6d7d70a3fd5
Connor Rhodes <connor@rhodes.contact>
2024-12-21 10:00:26 -0600

    support script args

--- rest_api_client/get-weather.rb
+++ rest_api_client/get-weather.rb
@@ -1,12 +1,15 @@
 require 'httparty'
 require 'json'
+require 'dotenv/load'
 require 'pry'
 
 hp = HTTParty
 
-api_key = '**********'
+api_key = ENV["OPEN_WEATHER_MAP_KEY"]
 
-geo = hp.get("http://api.openweathermap.org/geo/1.0/zip?zip=78758,US&appid=#{api_key}")
+zip_code = ARGV[0]
+
+geo = hp.get("http://api.openweathermap.org/geo/1.0/zip?zip=#{zip_code},US&appid=#{api_key}")
 
 weather = hp.get("https://api.openweathermap.org/data/2.5/weather?lat=#{geo["lat"]}&lon=#{geo["lon"]}&units=imperial&appid=#{api_key}")
 
